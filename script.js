0bc05d30edd9174e8396d7652674219a4812bed9
Pedro Nunes Custódio <pedronunesc07@gmail.com>
2025-09-24 21:55:14 -0300

    fix: Corrige o cálculo do índice e reativa a funcionalidade

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const conselhos = [
     "Acredite em você mesmo.", "O primeiro passo é o mais importante", "A persistência realiza o impossível.", " Aprenda algo novo hoje", "Seja gentil com todos que encontrar."
 ] 
-console.log("Arquivo de script carregado. Array de conselhos pronto.");
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho() {
+    const indice = Math.floor(Math.Random() * (conselhos.length));
+    conselhoTexto.TextContent = conselhos[indice];
+}
+novoConselhoBtn.addEventListener('click', gerarConselho);
