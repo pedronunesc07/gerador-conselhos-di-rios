7a3990c88530a5ffee47c3962f1648f954b9f43e
Pedro Nunes Custódio <pedronunesc07@gmail.com>
2025-09-24 21:20:38 -0300

    Criação e upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,4 @@
+const conselhos = [
+    "Acredite em você mesmo.", "O primeiro passo é o mais importante", "A persistência realiza o impossível.", " Aprenda algo novo hoje", "Seja gentil com todos que encontrar."
+] 
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
